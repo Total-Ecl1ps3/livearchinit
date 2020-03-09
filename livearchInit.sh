2a1eff3ad4e30b2c825a9d4603a46011fa5dc4ef
Total Ecl1ps3 <19960107+Total-Ecl1ps3@users.noreply.github.com>
2020-03-09 14:53:37 +0000

    update 1

--- livearchInit.sh
+++ livearchInit.sh
@@ -1,20 +1,92 @@
 #!/bin/bash
-echo "livearch init"
-function spacr() {
-    for i in {1..$1}; do
-        echo ""
-    done
+
+## Confirm
+
+function ync() {
+	while true
+	do
+		read -n 1 -r -p "[y/n]" input1
+
+		case $input1 in
+			[yY])
+		input1a="y"
+		break
+		;;
+			[nN])
+		input1a="n"
+		break
+		;;
+			*)
+		echo ""
+		echo "Invalid input..."
+		;;
+		esac
+	done
 }
+
+
+echo ""
+echo "livearch init"
+
+## Set keymap
 echo ""
 echo "Setting keymap"
-loadkeys no-latin1
+read -r -p "Set keymap: [no-latin1]" skm
+if [[ !-z $skm ]]
+then
+	loadkeys $skm
+	if [[ $? != "0" ]]
+	then
+		echo "Error setting keymap, try doing it manually!"
+	fi
+else
+	loadkeys no-latin1
+fi
+
 echo "Extending live root partition"
-read -p "Root fs size: "
-mount -o remount,size=4G
+while true
+do
+	echo "Set root partition size, in G if not specified. (1 = 1G, 1M = 1M) [#K/M/G]"
+	read -p "Root fs size: " setsizefs
+	if [[ $setsizefs =~ ^[0-9]+$ ]]
+	then
+		mount -o remount,size=${setsizefs}G /run/archiso/cowspace
+		break
+	elif [[ $setsizefs =~ ^[0-9]+[KMG]$ ]]
+	then
+		mount -o remount,size=${setsizefs} /run/archiso/cowspace
+		break
+	else
+		echo ""
+		echo "Error, try again!"
+	fi
+done
 
 echo ""
 echo "Fetching packages"
 pacman -Syy
-pacman -S pacman-contrib
+pacman -S pacman-contrib --noconfirm
+echo "Ranking mirrors based on speed"
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.back
+rankmirrors -n 5 -m 1.5 -v /etc/pacman.d/mirrorlist.back | tee /etc/pacman.d/mirrorlist
+
+echo "Installing basepackages"
+pacman -S i3 dmenu xorg-xinit xorg-server xorg-xrandr arandr firefox ttf-dejavu terminator tmux --noconfirm
+
+echo 'exec /usr/bin/i3' > ~/.xinitrc
+passwd
+
+echo ""
+echo "Start i3?"
+ync
+if [[ $input1a =~ [y] ]]
+then
+	startx
+fi
+
+
+
+
+
 
 
